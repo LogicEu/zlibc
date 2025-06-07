d65424719b8543c9e771692296e15ed44a737689
LogicEu <eugenioarteagaa@gmail.com>
2025-06-07 18:21:15 -0400

    Added ptrdiff_t definition to zstddef.h

--- src/include/zstddef.h
+++ src/include/zstddef.h
@@ -7,6 +7,12 @@
     typedef __ZSIZE_TYPE__ size_t;
 #endif /* SIZE */
 
+#ifndef __ZPTRDIFF_T
+    #define __ZPTRDIFF_T
+    #define __ZPTRDIFF_TYPE__ __PTRDIFF_TYPE__
+    typedef __ZPTRDIFF_TYPE__ ptrdiff_t;
+#endif /* PTRDIFF_T */
+
 #ifndef __ZWCHAR_T
     #define __ZWCHAR_T
     #define __ZWCHAR_TYPE__ __WCHAR_TYPE__
